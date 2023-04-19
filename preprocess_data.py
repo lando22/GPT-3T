9f0c7f6d285db8bcce3632176eef5dff0c49fe78
Landon Garrison <45245404+lando22@users.noreply.github.com>
2023-04-19 16:21:41 -0600

    Create preprocess_data.py

---
+++ preprocess_data.py
@@ -0,0 +1,20 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from torch.utils.data import DataLoader, Dataset
+from .train_model import TextDataset
+
+# Train a BPE tokenizer on the dataset
+def train_bpe_tokenizer(file_path, vocab_size=52000):
+    tokenizer = Tokenizer(BPE())
+    tokenizer.pre_tokenizer = Whitespace()
+    trainer = BpeTrainer(vocab_size=vocab_size, special_tokens=["<pad>", "<unk>"])
+    tokenizer.train(files=[file_path], trainer=trainer)
+    return tokenizer
+  
+def preprocess_data(datafile, sequence_length=128, batch_size=55, vocab_size=52000):
+    tokenizer = train_bpe_tokenizer(datafile, vocab_size)
+    dataset = TextDataset(datafile, tokenizer, sequence_length)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader, tokenizer
